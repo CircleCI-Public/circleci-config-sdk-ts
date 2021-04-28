7b16ae8450682de864cfc42899086515abb1f103
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-04-28 13:22:53 -0400

    Add store test results command

---
+++ src/lib/Components/Commands/Native/StoreTestResults.ts
@@ -0,0 +1,32 @@
+import { Command, CommandParameters } from "../index.types"
+
+/**
+ * Special step used to upload and store test results for a build. Test results are visible on the CircleCI web application, under each build’s “Test Summary” section. Storing test results is useful for timing analysis of your test suites.
+ * @param parameters - StoreTestResultsParameters
+ */
+export class StoreTestResults extends Command {
+	parameters: StoreTestResultsParameters
+	constructor(parameters: StoreTestResultsParameters) {
+		super("store_test_results")
+		this.parameters = parameters
+	}
+	/**
+	 * Generate StoreTestResults Command schema.
+	 * @returns The generated JSON for the StoreTestResults Command.
+	 */
+	generate(): StoreTestResultsCommandSchema {
+		return {store_test_results: {...this.parameters}} as StoreTestResultsCommandSchema
+	}
+}
+export default StoreTestResults
+export interface StoreTestResultsParameters extends CommandParameters {
+	/**
+	 * Path (absolute, or relative to your working_directory) to directory containing subdirectories of JUnit XML or Cucumber JSON test metadata files
+	 */
+	path: string
+
+}
+
+export interface StoreTestResultsCommandSchema {
+	store_test_results: StoreTestResultsParameters
+}
--- src/lib/Components/Commands/index.ts
+++ src/lib/Components/Commands/index.ts
@@ -2,6 +2,7 @@ import Run from "./Native/Run"
 import Checkout from "./Native/Checkout"
 import * as Cache from "./Native/Cache"
 import * as Workspace from "./Native/Workspace"
+import StoreTestResults from "./Native/StoreTestResults"
 import StoreArtifacts from "./Native/StoreArtifacts"
 
 
@@ -10,6 +11,7 @@ export {
 	Checkout,
 	Cache,
 	Workspace,
-	StoreArtifacts
+	StoreArtifacts,
+	StoreTestResults
 }
 
