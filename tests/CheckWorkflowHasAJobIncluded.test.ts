1fd6289520ee6a0da8b5491afc7e8069d7a0e6db
mrothstein74 <michael.rothstein@circleci.com>
2021-10-06 08:37:21 -0700

    tests to check for no job existing, and workflow containing no job pass of true

--- tests/CheckWorkflowHasAJobIncluded.test.ts
+++ tests/CheckWorkflowHasAJobIncluded.test.ts
@@ -1,10 +1,13 @@
 import * as CircleCI from '../src/index';
+
+// This is a negative test that will pass if no job has been included in the workflow
+
 // Enforce local testing
 delete process.env.CIRCLECI;
-describe('Check workflow includes at least one job', () => {
+describe('Check is workflow includes no jobs', () => {
   const myWorkflow = new CircleCI.Workflow('my-workflow');
 
-  it('Workflow should have at least one job included', () => {
-    expect(myWorkflow.jobs.length).toBeGreaterThanOrEqual(1);
+  it('Workflow should have no jobs included', () => {
+    expect(myWorkflow.jobs.length).toEqual(0);
   });
 });
