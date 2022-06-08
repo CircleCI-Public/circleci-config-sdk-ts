4de82ef3d2af2bd00074d2d12406a39392ac7f96
Jaryt Bustard <jarytbustard@gmail.com>
2022-06-08 17:02:39 -0400

    chore: remove test file

--- publish.sh
+++
@@ -1,9 +0,0 @@
-if [ -n "$CIRCLE_TAG" ]; then
-    PATTERN="[0-9]*\.[0-9]*\.[0-9]*(-(alpha|beta))"
-    [[ $CIRCLE_TAG =~ $PATTERN ]]
-    
-    if [ -n "${BASH_REMATCH[2]}" ]; then
-        echo "Publishing with ${BASH_REMATCH[2]} tag"
-        set -- "$@" --tag "${BASH_REMATCH[2]}"
-    fi
-fi
