e0183bab3c72a3962ccf4b522797ac7b8ec1770c
Jaryt Bustard <jarytbustard@gmail.com>
2022-06-16 17:48:05 -0400

    feat: Add single key support for restore_cache

--- src/lib/Components/Commands/schemas/Native/Cache/Restore.schema.ts
+++ src/lib/Components/Commands/schemas/Native/Cache/Restore.schema.ts
@@ -2,17 +2,25 @@ import { SchemaObject } from 'ajv';
 
 const RestoreSchema: SchemaObject = {
   $id: '#/commands/native/restore_cache',
-  type: 'object',
-  required: ['keys'],
-  properties: {
-    keys: {
-      type: 'array',
-      minItems: 1,
-      items: {
-        type: 'string',
+  oneOf: [
+    {
+      type: 'object',
+      required: ['keys'],
+      properties: {
+        keys: {
+          type: 'array',
+          minItems: 1,
+          items: {
+            type: 'string',
+          },
+        },
       },
     },
-  },
+    {
+      type: 'string',
+      required: ['key'],
+    },
+  ],
 };
 
 export default RestoreSchema;
