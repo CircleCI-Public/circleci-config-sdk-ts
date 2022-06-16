6ddc2a12bf3ec5752b89cd6e4c64e0c2206cd044
Jaryt Bustard <jarytbustard@gmail.com>
2022-06-16 11:38:46 -0400

    feat: WorkflowJob name getter

--- src/lib/Components/Workflow/exports/WorkflowJobApproval.ts
+++ src/lib/Components/Workflow/exports/WorkflowJobApproval.ts
@@ -5,17 +5,21 @@ import {
 import { WorkflowJobAbstract } from './WorkflowJobAbstract';
 
 export class WorkflowJobApproval extends WorkflowJobAbstract {
-  name: string;
+  private _name: string;
 
   constructor(name: string, parameters?: WorkflowJobParameters) {
     super({ ...parameters, type: 'approval' });
 
-    this.name = name;
+    this._name = name;
   }
 
   generate(): WorkflowJobShape {
     return {
       [this.name]: this.generateContents(),
     };
+  }
+
+  get name(): string {
+    return this._name;
   }
 }
