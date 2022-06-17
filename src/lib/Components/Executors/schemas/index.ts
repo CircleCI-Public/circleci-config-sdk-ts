54dee7b90aaaed002ea32411e0e3f4342ae137fc
Jaryt Bustard <jarytbustard@gmail.com>
2022-06-17 14:24:24 -0400

    feat: export schemas

---
+++ src/lib/Components/Executors/schemas/index.ts
@@ -0,0 +1,15 @@
+import DockerExecutableSchema from './DockerExecutable.schema';
+import ExecutorSchema from './Executor.schema';
+import MachineExecutableSchema from './MachineExecutable.schema';
+import MacOSExecutableSchema from './MacosExecutable.schema';
+import WindowsExecutableSchema from './WindowsExecutable.schema';
+import * as reusable from './ReusableExecutable.schema';
+
+export {
+  reusable,
+  DockerExecutableSchema,
+  ExecutorSchema,
+  MachineExecutableSchema,
+  MacOSExecutableSchema,
+  WindowsExecutableSchema,
+};
--- src/lib/Components/Parameters/schemas/ComponentParameterLists.schema.ts
+++ src/lib/Components/Parameters/schemas/ComponentParameterLists.schema.ts
@@ -1,5 +1,16 @@
 import { SchemaObject } from 'ajv';
-import { genParameterListSchema } from '.';
+
+const genParameterListSchema = (type: string): SchemaObject => {
+  return {
+    $id: `#/parameters/${type}ParameterList`,
+    type: 'object',
+    description:
+      'https://circleci.com/docs/2.0/reusing-config/#using-the-parameters-declaration\n\nA map of parameter keys.',
+    patternProperties: {
+      '^[a-z][a-z0-9_-]+$': { $ref: `#/parameters/${type}Parameters` },
+    },
+  };
+};
 
 const JobParameterListSchema: SchemaObject = genParameterListSchema('Job');
 const CommandParameterListSchema: SchemaObject =
