d31ffc61ef97805cea99772f2715a7caa64865bc
Kyle.Tryon <Kyle.Tryon@outlook.com>
2022-08-08 21:15:33 -0400

    feat!: remove parser package

--- packages/ConfigParser/.prettierrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  proseWrap: 'always',
-  trailingComma: 'all',
-};
--- packages/ConfigParser/lib/Components/Commands/schemas/Reusable/index.ts
+++
@@ -1,4 +0,0 @@
-import ReusableCommandSchema from './ReusableCommand.schema';
-import ReusedCommandSchema from './ReusedCommand.schema';
-
-export { ReusableCommandSchema, ReusedCommandSchema };
