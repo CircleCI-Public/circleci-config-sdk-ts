46cb714805635beef06e402d1f3ddeab8f15f225
Jaryt Bustard <jarytbustard@gmail.com>
2022-09-18 03:03:41 -0400

    feat: added extended resource classes (#145)
    * feat: added extended resource classes
    
    * chore: linting

--- src/lib/Components/Executors/types/DockerExecutor.types.ts
+++ src/lib/Components/Executors/types/DockerExecutor.types.ts
@@ -1,6 +1,6 @@
 import { StringParameter } from '../../Parameters/types';
 import { DockerImageShape } from '../exports/DockerImage';
-import { AnyResourceClass } from './Executor.types';
+import { AnyResourceClassBase } from './Executor.types';
 /**
  * A JSON representation of the Docker Executor Schema
  * To be converted to YAML
@@ -15,4 +15,4 @@ export type DockerImageMap = {
  * The available Docker Resource Classes.
  * @see {@link https://circleci.com/docs/2.0/configuration-reference/#docker-executor} for specifications of each class.
  */
-export type DockerResourceClass = AnyResourceClass;
+export type DockerResourceClass = AnyResourceClassBase;
